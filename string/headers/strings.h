74b324a2bd279cebe51c7073324119b30703bb12
Ismail <ujinmops@gmail.com>
2020-08-03 05:11:39 +0500

    last functionaliti change

--- string/headers/strings.h
+++ string/headers/strings.h
@@ -12,15 +12,30 @@ typedef struct strings
     int mem_free;
 }string_t;
 /* the function can be called with size 0 then it will be calculate the size itself  */
-string_t *str_init(const char *str, int str_size);
-/* if str_size equal 0 or > str->used then str_size = sre->used  */
-string_t *with_str_init(const string_t *str, int str_size);
-string_t *strcopy_new(const string_t *src);
-void strcopy(string_t *dst, const string_t *src);
+string_t *str_init(const char *, int);
+/* if str_size equal 0 or > str->used then str_size = str->used  */
+string_t *with_str_init(const string_t *, int);
+void strcopy(string_t **out, const string_t *);
 /* record new C string  */
-void new_string(const char *new_str, string_t *str_ptr);
-int str_compare(const string_t* str1, const string_t* str2);
-int strn_compare(const string_t* str1, const string_t*, int n);
-void str_reverse(char* str, int str_size);
+/* copy first n characters  */
+void strn_copy(string_t **out, const string_t *, int);
+char *new_string(const string_t *);
+/* returns the addres of the character in a string  */
+char *str_char(const string_t *, char);
+/* returns the addres of the last character in a string */
+char *strr_char(const string_t *, char);
+/* reduce the amount of memory on the string */
+void str_shrink(string_t *, int);
+/* increase the amount of memory on the string */
+void str_grow(string_t *, int);
+/* finds the first occurrence of a string */
+int str_str(const string_t *, const string_t *, char **out);
+/* appends src string to dst string to the end point */
+void str_cat(const string_t *, string_t *);
+/* appends src string to dst string in the n positiont */
+void strn_cat(const string_t *, string_t *, int);
+int str_compare(const string_t*, const string_t*);
+int strn_compare(const char*, const char*, int);
+void str_reverse(char *, int);
 
 #endif
--- string/strings.c
+++ string/strings.c
@@ -1,9 +1,10 @@
 #include "headers/strings.h"
 #include <stdlib.h>
 
-enum{ null_symbol = 1 };
+enum{ null_symbol = 1, equal = 0, larger = 1, less = -1, find = 1, not_find = 0 };
 
 static void ch_copy(const char *src, char *dst, int size);
+static void str_shift(string_t *str_to_shft, int position, int shft_count);
 
 string_t *str_init(const char *str, int size)
 {
@@ -48,12 +49,150 @@ string_t *with_str_init(const string_t *str, int str_size)
 
 int str_compare(const string_t *src_str, const string_t *dst_str)
 {
-    int i;
-    for(i = 0; i < (src_str->used - 1); i++)
-        if(src_str->data != dst_str->data)
+    int compare_res = equal, i;
+    for(i = 0; i < (src_str->used - null_symbol); i++)
+    {
+        if(src_str->data[i] == dst_str->data[i])
+            continue;
+        else
         {
-
+            if(dst_str->data[i] == '\0')
+                return larger;
+            else
+                return src_str->data[i] > dst_str->data[i] ? larger : less;
         }
+    }
+    return compare_res;
+}
+
+char *new_string(const string_t *str)
+{
+    char *new_str = malloc(sizeof(char) * str->used);
+    if(new_str)
+    {
+        ch_copy(str->data, new_str, str->used);
+        return new_str;
+    }
+    return NULL;
+}
+
+void strcopy(string_t **dst, const string_t *src)
+{
+    *dst = malloc(sizeof(string_t));
+    (*dst)->data = malloc(sizeof(char) * src->used);
+    if(*dst)
+    {
+        ch_copy(src->data, (*dst)->data, src->used);
+        (*dst)->max_length = src->max_length;
+        (*dst)->used = src->used;
+        (*dst)->mem_free = src->mem_free;
+    }
+}
+
+void strn_copy(string_t **dst, const string_t *src, int size)
+{
+    *dst = malloc(sizeof(string_t));
+    if(*dst)
+    {
+        (*dst)->data = malloc(sizeof(char) * size + null_symbol);
+        if((*dst)->data)
+        {
+            ch_copy(src->data, (*dst)->data, size);
+            (*dst)->used = size;
+            (*dst)->max_length = size;
+            (*dst)->mem_free = size;
+        }
+    }
+}
+
+char *str_char(const string_t *str_src, char ch)
+{
+    int iter;
+    for(iter = 0; iter < str_src->used; iter++)
+    {
+        if(str_src->data[iter] == ch)
+            return &(str_src->data[iter]);
+    }
+    return NULL;
+}
+
+char *strr_char(const string_t *str, char ch)
+{
+    int iter;
+    char *res = NULL;
+    for(iter = 0; iter < str->used - null_symbol; iter++)
+        res = str->data[iter] == ch ? &(str->data[iter]) : res;
+    return res;
+}
+
+void str_shrink(string_t *str, int count)
+{
+    int iter, new_size = str->used - count;
+    char *new_str = malloc(sizeof(char) * new_size);
+    for(iter = 0; iter < new_size - null_symbol; iter++)
+        new_str[iter] = str->data[iter];
+    new_str[iter] = '\0';
+    free(str->data);
+    str->data = new_str;
+    str->max_length = new_size;
+    str->used = new_size;
+}
+
+void str_grow(string_t *str, int count)
+{
+    int new_size = str->used + count;
+    str->data = realloc(str->data, new_size);
+    str->max_length = new_size;
+    str->mem_free = new_size - str->used;
+}
+
+void str_cat(const string_t *src, string_t *dst)
+{
+    int size_now = dst->used;
+    str_grow(dst, src->used - null_symbol);
+    ch_copy(src->data, &(dst->data[size_now - null_symbol]), src->used);
+}
+
+void strn_cat(const string_t *src, string_t *dst, int position)
+{
+    int src_size = src->used - null_symbol;
+    str_grow(dst, src_size);
+    str_shift(dst, position, src_size);
+    char *src_str = &(dst->data[position]);
+    for(int iter = 0; iter < src_size; iter++, src_str++)
+        *src_str = src->data[iter];
+}
+
+int str_str(const string_t *haystack, const string_t *needle, char **out_str_addres)
+{
+    int i = 0, j = 0;
+    for(; haystack->data[i]; j++, i++)
+    {
+        if(haystack->data[i] != needle->data[j])
+        {
+            i = j > 0 ? i - 1 : i;      /* <------ this construction returns us on the next */
+            j = -1;                     /* iteration to the same character in order to check it at the beginning of the string  */
+            continue;
+        }else
+        if(j + 1 == (needle->used - null_symbol))
+        {
+            if(out_str_addres)
+            {
+                *out_str_addres = haystack->data + i - j;
+            }
+            return find;
+        }
+    }
+    return not_find;
+}
+
+int strn_compare(const char *src_str, const char *dst_str, int size)
+{
+    if(!size)
+        return equal;
+    return *src_str == *dst_str ?
+            strn_compare(src_str + 1, dst_str + 1, size - 1)
+            : *src_str > *dst_str ? larger : less;
 }
 
 void str_reverse(char* str, int size)
@@ -67,8 +206,18 @@ void str_reverse(char* str, int size)
     }
 }
 
+static void str_shift(string_t *str_to_shft, int position, int shft_count)
+{
+    int end_pos = str_to_shft->used;
+    for(; end_pos >= position; end_pos--)
+    {
+        for(int iter = 0, indx = end_pos; iter < shft_count; iter++, indx++)
+            str_to_shft->data[indx + 1] = str_to_shft->data[indx];
+    }
+}
+
 static void ch_copy(const char *src, char *dst, int size)
 {
     for(int i = 0; i < size; i++, *dst = *src, src++, dst++);
-    *(dst + 1) = '\0';
+    *dst = '\0';
 }
